bcc560c5949c79fa7765e944a20f1f25721190e8
Joe Chapman <contemno82@gmail.com>
2018-02-01 21:24:30 +0000

    removed notify functions

--- app/middleware/sendBookedEmail.js
+++ app/middleware/sendBookedEmail.js
@@ -2,25 +2,26 @@ const notify = require('../services/notify/notify');
 const mainConfig = require('../config/main');
 
 module.exports = (req, res, next) => {
-    let param = encodeURIComponent(res.locals.booking._id);
-    const personalisation = {
-        cancelLink: `${process.env.NODE_URI}/cancel/${param}`,
-        name: req.body.name,
-        roomName: res.locals.room.name,
-        bookedDate: res.locals.today.dateYear.toString(),
-        bookedFrom: res.locals.today.dateYear.toString(),
-        bookedUntil: res.locals.today.dateYear.toString(),
-        bookingDescription: req.body.reason
-    };
-    console.log(personalisation.cancelLink);
-    notify.sendEmail(mainConfig.bookedTemplate, req.body.email, {personalisation: personalisation})
-    .then(response => {
-        console.log(response.body.id);
-        return next();
-    })
-    .catch(err => {
-        console.log(err.message);
-        res.locals.emailFailure = `Failure:<br />error: ${err.error.errors[0].message}`;
-        return next();
-    });
+    return next();
+    // let param = encodeURIComponent(res.locals.booking._id);
+    // const personalisation = {
+    //     cancelLink: `${process.env.NODE_URI}/cancel/${param}`,
+    //     name: req.body.name,
+    //     roomName: res.locals.room.name,
+    //     bookedDate: res.locals.today.dateYear.toString(),
+    //     bookedFrom: res.locals.today.dateYear.toString(),
+    //     bookedUntil: res.locals.today.dateYear.toString(),
+    //     bookingDescription: req.body.reason
+    // };
+    // console.log(personalisation.cancelLink);
+    // notify.sendEmail(mainConfig.bookedTemplate, req.body.email, {personalisation: personalisation})
+    // .then(response => {
+    //     console.log(response.body.id);
+    //     return next();
+    // })
+    // .catch(err => {
+    //     console.log(err.message);
+    //     res.locals.emailFailure = `Failure:<br />error: ${err.error.errors[0].message}`;
+    //     return next();
+    // });
 };
--- app/middleware/sendInviteEmail.js
+++ app/middleware/sendInviteEmail.js
@@ -2,25 +2,26 @@ const notify = require('../services/notify/notify');
 const mainConfig = require('../config/main');
 
 module.exports = (req, res, next) => {
+    return next();
     // if(res.locals.user !== null) {
     //     res.locals.errors = {email: {msg: 'User already has an account'}};
     //     return next();
     // }
-    console.log('shoulnt run');
-    let param = encodeURIComponent(res.locals.newUserToken);
-    let link = `${process.env.NODE_URI}/new-user?token=${param}`;
-    const personalisation = {
-        link: link
-    };
-    notify.sendEmail(mainConfig.inviteTemplate, req.body.email, {personalisation: personalisation})
-    .then(response => {
-        res.locals.invited = true;
-        console.log(response.body.id);
-        return next();
-    })
-    .catch(err => {
-        console.log(err.message);
-        res.locals.emailFailure = `Failure:<br />error: ${err.error.errors[0].message}`;
-        return next();
-    });
+    // console.log('shoulnt run');
+    // let param = encodeURIComponent(res.locals.newUserToken);
+    // let link = `${process.env.NODE_URI}/new-user?token=${param}`;
+    // const personalisation = {
+    //     link: link
+    // };
+    // notify.sendEmail(mainConfig.inviteTemplate, req.body.email, {personalisation: personalisation})
+    // .then(response => {
+    //     res.locals.invited = true;
+    //     console.log(response.body.id);
+    //     return next();
+    // })
+    // .catch(err => {
+    //     console.log(err.message);
+    //     res.locals.emailFailure = `Failure:<br />error: ${err.error.errors[0].message}`;
+    //     return next();
+    // });
 };
